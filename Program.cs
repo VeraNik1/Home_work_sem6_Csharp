3523e54c133d82d14a3de70d70fb2565320521e0
Vera <solnce288888@gmail.com>
2022-10-18 23:02:16 +0300

    num 43

--- Program.cs
+++ Program.cs
@@ -7,23 +7,38 @@
 Console.WriteLine("Сколько чисел будем проверять?: ");
 int M = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine($"Вы ввели {CountPositiveNums(M)} положительных числа из {M}");
-
-int CountPositiveNums(int N){
-    int count = 0;
-    int temp;
-    for(int i = 0; i < N; i++){
-        Console.WriteLine($"Введите {i + 1}-й элемент из {N}: ");
-        temp = int.Parse(Console.ReadLine()!);
-        if(temp > 0){
-            count++;
+int count = 0;
+int temp;
+for(int i = 0; i < M; i++){
+    Console.WriteLine($"Введите {i + 1}-й элемент из {M}: ");
+    temp = int.Parse(Console.ReadLine()!);
+    if(temp > 0){
+        count++;
         }
     }
-    return count;
-}
+
+Console.WriteLine($"Количество положительных чисел из {M} введенных: {count}");
 
 
-
-/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+Console.WriteLine("Введите значение b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+if (k1 == k2){
+    Console.WriteLine($"Прямые, заданные уравнениями y = {k1} * x + ({b1}) и y = {k2} * x + ({b2}) параллельны");
+    }
+else{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Прямые, заданные уравнениями y = {k1} * x + ({b1}) и y = {k2} * x + ({b2}), пересекаются в точке ({x}, {y})");
+    }
+
